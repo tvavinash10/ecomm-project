912a084b087d03fafb4a2a1a6252c9c31490c79c
Avinash.T.V <tvavinash10@gmail.com>
2022-11-10 14:06:08 +0530

    Logout and Routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  menuType: String ='default';
+  menuType: string ='default';
+  sellerName: string ='';
   constructor(private route: Router) { }
 
   ngOnInit(): void {    
@@ -17,12 +18,21 @@ export class HeaderComponent implements OnInit {
         if(localStorage.getItem('seller') && val.url.includes('seller')){
           console.warn("in seller area")
           this.menuType="seller"
+          if(localStorage.getItem('seller')){
+            let sellerStore = localStorage.getItem('seller');
+            let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+            this.sellerName = sellerData.name;
+          }
         } else {
           console.warn("outside seller area")
           this.menuType="default"
         }
       }
-    })
+    });
+  }
+  logout(){
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
   }
 
 }
