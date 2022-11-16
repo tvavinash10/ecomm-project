4f91aed9fd5fee4622e3f17d0c5651679a4312a4
Avinash.T.V <tvavinash10@gmail.com>
2022-11-16 17:25:59 +0530

    UI for Update Product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
 
 const routes: Routes = [
   {
@@ -23,6 +24,11 @@ const routes: Routes = [
   {
     path : 'seller-add-product',
     component:SellerAddProductComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path : 'seller-update-product/:id',
+    component:SellerUpdateProductComponent,
     canActivate: [AuthGuard]
   }
 ];
