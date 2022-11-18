ab5a0b00b11808e97134ef8d1d7fdfb07fa89be5
Avinash.T.V <tvavinash10@gmail.com>
2022-11-19 00:01:43 +0530

    Display Product in Bootstrap Carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  constructor() { }
+export class HomeComponent implements OnInit {  
+  popularProducts: undefined | product[];
+  constructor(private product: ProductService) { }
 
   ngOnInit(): void {
+    this.product.popularProducts().subscribe((data) => {
+      console.warn(data);
+      this.popularProducts = data;
+    })
   }
 
 }
