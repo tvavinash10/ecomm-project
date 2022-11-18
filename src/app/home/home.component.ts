fc7d6fef56ce1fcbc63d0973d6c9f7312bfef880
Avinash.T.V <tvavinash10@gmail.com>
2022-11-18 20:30:08 +0530

    Add Bootstrap in Angular and Use Carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   constructor() { }
 
   ngOnInit(): void {
